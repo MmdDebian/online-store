d2615ed8a732b09fdcd2b0c942d78a67976bf313
mohamad <mohamadmirzaei.ir@gmail.com>
2022-08-25 01:38:20 +0430

    refactoring in main.ts file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , {cors : true});
   app.use(cors())
